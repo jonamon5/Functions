4c1e58426deaa530fe5e483c1e894bbdf4d90db6
Jonathan Chiang <chiaj413@newschool.edu>
2025-03-19 14:34:09 -0400

    link dummydata and display it on home page

--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -2,10 +2,20 @@ import dummyData from '../data/dummyData.json';
 
 export default function Home() {
 
+const data = dummyData;
+
+  console.log("data",data);
+
   return (
 
-    <div style={{ padding: '20px', fontFamily: 'Arial, sans-serif' }}>
+    <div className="title">
       <h1>CPAP Sleep Apnea Insights</h1>
+      <ul>
+        <li>Average AHI per Night: {data.summary.averageAHIperNight}</li>
+        <li>Average Pressure: {data.summary.averagePressure}</li>
+        <li>Average Leak Rate: {data.summary.averageLeakRate}</li>
+        <li>Daily Usage Hours:{data.summary.dailyUsageHours}</li>
+      </ul>
     </div>
     
   );
