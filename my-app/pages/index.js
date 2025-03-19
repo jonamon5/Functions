07c93fc5c45b54ed2ce4c81a80a87dbcbdf2878b
Jonathan Chiang <chiaj413@newschool.edu>
2025-03-19 19:19:26 -0400

    add css

--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -2,21 +2,39 @@ import dummyData from '../data/dummyData.json';
 
 export default function Home() {
 
-const data = dummyData;
-
-  console.log("data",data);
+  const data = dummyData;
+  console.log("data", data);
 
   return (
 
-    <div className="title">
-      <h1>CPAP Sleep Apnea Insights</h1>
-      <ul>
-        <li>Average AHI per Night: {data.summary.averageAHIperNight}</li>
-        <li>Average Pressure: {data.summary.averagePressure}</li>
-        <li>Average Leak Rate: {data.summary.averageLeakRate}</li>
-        <li>Daily Usage Hours:{data.summary.dailyUsageHours}</li>
-      </ul>
+    <div>
+      <h1 className="title">SleepVantage</h1>
+      <p>SleepVantage is a smart sleep apnea assistant that provides actionable insights for you, improving your CPAP therapy </p>
+
+      <div className='core-stats-section'>
+
+        <div className='ahi'>
+          <p className='stats-title'> Average AHI per Night:</p>
+          <p className='stats'>{data.summary.averageAHIperNight}</p>
+        </div>
+
+        <div className='pressure'>
+          <p className='stats-title'>Average Pressure</p>
+          <p className='stats'>{data.summary.averagePressure}</p>
+        </div>
+
+        <div className='leak-rate'>
+          <p className='stats-title'>Average Leak Rate</p>
+          <p className='stats'>{data.summary.averageLeakRate}</p>
+        </div>
+
+        <div className='usage'>
+          <p className='stats-title'>Daily Usage Hours</p>
+          <p className='stats'>{data.summary.dailyUsageHours}</p>
+        </div>
+      </div>
+
     </div>
-    
+
   );
 }
