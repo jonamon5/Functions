07ee463c3992ade53727659648a86d3a5a3a0645
Jonathan Chiang <chiaj413@newschool.edu>
2025-03-22 15:44:51 -0400

    GET request for all CPAP machine data
    plotted the first endpoints down

--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -1,29 +1,38 @@
 import dummyData from '../data/dummyData.json';
 import axios from 'axios';
 
-// API GET REQUEST FOR CURRENT USER DATA
-export async function getServerSideProps() {
-  const response = await axios.get('https://sleephq.com/api/v1/me', {
+// FFETCH MACHINE DATA
+async function fetchMachineDates() {
+  const response = await axios.get('https://sleephq.com/api/v1/machines/47720/machine_dates?sort_order=desc&page=1&per_page=100', {
     headers: {
       'accept': 'application/vnd.api+json',
       'authorization': 'Bearer r5edXo23zXY1DrS_yUjLREz0Jiu66PKYFHuEdua0pY4'
     }
   });
+  return response.data;
+}
 
-  const userData = response.data;
-  console.log('userData', userData);
+// API GET REQUEST FOR MACHINE DATES
+export async function getServerSideProps() {
+  const machineDatesData = await fetchMachineDates();
+
+  console.log('machineDatesData', machineDatesData);
 
   return {
-    props: { userData } // Return userData as a prop
+    props: { machineDatesData }
   };
 }
 
+
+
 // DISPLAY DATA ON HOME PAGE 
-export default function Home({ userData }) {
+export default function Home({ machineDatesData }) {
 
-  console.log('userData', userData.data);
+  console.log('machineDatesData', machineDatesData.data);
+  console.log('data', machineDatesData);
+  // Each ID is a different date, 100 in total 
 
-  const data = dummyData;
+  const data = machineDatesData.data;
  
   return (
 
@@ -38,23 +47,24 @@ export default function Home({ userData }) {
 
         <div className='ahi'>
           <p className='stats-title'> Average AHI per Night:</p>
-          <p className='stats'>{data.summary.averageAHIperNight}</p>
+          <p className='stats'>{(data[0].attributes.ahi_summary.total).toFixed(2)}</p>
         </div>
 
         <div className='pressure'>
           <p className='stats-title'>Average Pressure</p>
-          <p className='stats'>{data.summary.averagePressure}</p>
+          <p className='stats'>{(data[0].attributes.pressure_summary.av).toFixed(2)}</p>
         </div>
 
         <div className='leak-rate'>
           <p className='stats-title'>Average Leak Rate</p>
-          <p className='stats'>{data.summary.averageLeakRate}</p>
+          <p className='stats'>{(data[0].attributes.leak_rate_summary.av).toFixed(2)}</p>
         </div>
 
         <div className='usage'>
-          <p className='stats-title'>Daily Usage Hours</p>
-          <p className='stats'>{data.summary.dailyUsageHours}</p>
+          <p className='stats-title'>Average EPAP</p>
+          <p className='stats'>{(data[0].attributes.epap_summary.av).toFixed(2)}</p>
         </div>
+        
       </div>
 
     </div>
